06d7464fd0043367f88eaa4245a1a29af1009952
parkoriann <debiuti2@gmail.com>
2023-12-02 05:48:44 +0100

    feat(education): education view model - VIDA-29

---
+++ lib/view/education/education_view_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../model/education.dart';
+
+class EducationViewModel with ChangeNotifier {
+
+  String playerName;
+  List<Education> educations;
+
+}

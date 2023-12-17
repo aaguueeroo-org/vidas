999ab05f8d7337ca1b2db0df882a40e0f4bb51c1
parkoriann <debiuti2@gmail.com>
2023-12-17 18:15:24 +0100

    feat(education): education view not visible until 3 yo - VIDA-37

--- lib/view/education/education_view.dart
+++ lib/view/education/education_view.dart
@@ -1,54 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:vidas/view/education/widgets/education_action_button.dart';
 import 'package:vidas/view/education/widgets/education_info.dart';
 import 'package:vidas/view/education/list_of_titles/education_list.dart';
 
+import 'education_view_model.dart';
 
 class EducationView extends StatelessWidget {
   const EducationView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Provider.of<EducationViewModel>(context);
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.onPrimary,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        actions: const [
-          // IconButton(
-          //   // height: 10,
-          //   onPressed: () => controller.showOptionsDialog(context),
-          //   icon: Icon(
-          //     Icons.settings_outlined,
-          //     size: width / 12,
-          //   ),
-          //   color: Theme.of(context).colorScheme.onSecondary,
-          // ),
-        ],
+        actions: const [],
       ),
-      body: const Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(height: 10),
-          EducationInfo(),
-          SizedBox(height: 20),
-          EducationActionButton(),
-          // const SizedBox(
-          //   height: 150,
-          //   child: LogContainer(),
-          // ),
-          SizedBox(height: 20),
-          Expanded(child: EducationList()),
-          SizedBox(height: 30),
-          // ElevatedButton(
-          //   onPressed: () => controller.showActionsDialog(context),
-          //   child: const Text('Actions'),
-          // ),
-          // const LogsAndButtons(),
-        ],
-      ),
+      body: controller.age < 3
+          ? const Center(
+              child: Text('Too young'),
+            )
+          : const Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                EducationInfo(),
+                EducationActionButton(),
+                Expanded(child: EducationList()),
+              ],
+            ),
     );
   }
 }
